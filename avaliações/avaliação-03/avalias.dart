572ea40c71f5756addcd01e3c4fd671fb0558e4e
thiago-cdb <fthiagocordeirodebrito@gmail.com>
2025-05-20 14:53:36 -0300

    add avalias -03 chama porra

---
+++ avaliações/avaliação-03/avalias.dart
@@ -0,0 +1,20 @@
+import 'dart:io';
+import 'dart:async';
+import 'dart:isolate';
+void main() async {
+final receivePort = ReceivePort();
+await Isolate.spawn(doAsyncOperation, receivePort.sendPort);
+// Executando outras tarefas enquanto aguarda a conclusão da operação assíncrona
+  print('Iniciando outras tarefas...');
+  await Future.delayed(Duration(seconds: 1));
+  print('Continuando outras tarefas...');
+
+  // Recebendo o resultado da operação assíncrona
+  final result = await receivePort.first;
+  print('nome: $result');
+}
+void doAsyncOperation(SendPort sendPort) async {
+  // Executando uma operação assíncrona em um isolate separado
+  final result = 'Riquelmy da Silva Ricarte';
+  sendPort.send(result);
+}
