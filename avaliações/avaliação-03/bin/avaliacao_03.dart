77665a7ccd40928f67b539561138989675de99a5
Riquelmy77 <riquelmy.ricarte08@aluno.ifce.edu.br>
2025-05-21 00:48:14 -0300

    Adicionando projeto dart avaliação-03

--- avaliações/avaliação-03/bin/avaliacao_03.dart
+++ avaliações/avaliação-03/bin/avaliacao_03.dart
@@ -1,10 +1,11 @@
 import 'dart:io';
 import 'dart:async';
 import 'dart:isolate';
+
 void main() async {
-final receivePort = ReceivePort();
-await Isolate.spawn(doAsyncOperation, receivePort.sendPort);
-// Executando outras tarefas enquanto aguarda a conclusão da operação assíncrona
+  final receivePort = ReceivePort();
+  await Isolate.spawn(doAsyncOperation, receivePort.sendPort);
+  // Executando outras tarefas enquanto aguarda a conclusão da operação assíncrona
   print('Iniciando outras tarefas...');
   await Future.delayed(Duration(seconds: 1));
   print('Continuando outras tarefas...');
@@ -13,6 +14,7 @@ await Isolate.spawn(doAsyncOperation, receivePort.sendPort);
   final result = await receivePort.first;
   print('nome: $result');
 }
+
 void doAsyncOperation(SendPort sendPort) async {
   // Executando uma operação assíncrona em um isolate separado
   final result = 'Riquelmy da Silva Ricarte';
